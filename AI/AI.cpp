d70bcd0b80bdf95f529cb2c30e751d9d8862eac8
MichelleQueen <miche0926@gmail.com>
2022-07-04 17:21:04 +0800

    okla

--- AI/AI.cpp
+++ AI/AI.cpp
@@ -23,7 +23,7 @@ void AI::update(list<Object*> object_list){
     for (auto it = object_list.begin(); it != object_list.end(); it++) {
         if (dynamic_cast<Player*> (*it)  != 0 ){ //find player
             this->img = ImageProcess::load_bitmap_at_size(path.c_str(), w, h);
-            if (this->img = "./image/ship1.png";){ //check if is P1 (idk how to write
+            if (this->img == "./image/ship1.png";){ //check if is P1 (idk how to write
                 P1 = dynamic_cast<Player*>(*it);  
             }
             else{
@@ -35,12 +35,12 @@ void AI::update(list<Object*> object_list){
     if(P1->x   <   P2->x){
         action[ALLEGRO_KEY_LEFT]=1;
     }
-    
+
     if(P1->x   >   P2->x){
         action[ALLEGRO_KEY_RIGHT]=1;
     }
 
-    if(P1->x   =   P2->x){
+    if(P1->x   ==   P2->x){
             // when shoot, create new bullet object in front of AI
         if(key_state[ALLEGRO_KEY_SPACE] && this->P1->energy >= 20 && this->P1->bullet_cool == 0){
             this->P1->bullet_cool = 5;
